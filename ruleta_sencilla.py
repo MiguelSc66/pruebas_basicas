3bcbe2c45c2e262e0f151fe0641bff37c1d2eb93
MiguelSc66 <112530966+MiguelSc66@users.noreply.github.com>
2023-05-06 14:04:29 -0300

    Create ruleta_sencilla.py

---
+++ ruleta_sencilla.py
@@ -0,0 +1,5 @@
+import random 
+
+pj = ['Astra', 'Breach', 'Brimstone', 'Chamber', 'Cypher', 'Fade', 'Gekko', 'Harbor', 'Jett', 'KAYO', 'Killjoy', 'Neon', 'Omen', 'Phoenix', 'Raze', 'Reyna', 'Sage', 'Skye', 'Sova', 'Viper', 'Yoru']
+pj_elejido = random.choice(pj)
+print("el personaje que toco es:", pj_elejido)
